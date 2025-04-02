2ab248bfb835a109cd38483332da38ef6b36be87
bbbbbooobbb <1415661217@qq.com>
2025-04-02 17:24:13 +0800

    Update validator.py
    Signed-off-by: bbbbbooobbb <1415661217@qq.com>

--- ultralytics/engine/validator.py
+++ ultralytics/engine/validator.py
@@ -146,7 +146,7 @@ class BaseValidator:
             self.device = trainer.device
             self.data = trainer.data
             # Force FP16 val during training
-            self.args.half = self.device.type != "cpu" and trainer.amp
+        
             model = trainer.ema.ema or trainer.model
             model = model.half() if self.args.half else model.float()
             # self.model = model
